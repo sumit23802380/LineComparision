6d06b00183847fea0321916ed04d8719d3882704
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 20:01:12 +0530

    Refactor : Renamed file and project names

--- src/LineComparision.java
+++
@@ -1,8 +0,0 @@
-/*
-    *@desc : Line Comparision Computation Class
- */
-public class LineComparision {
-    public static void main(String[] args) {
-        System.out.println("Welcome to line Comparision Computation");
-    }
-}
---
+++ src/LineComparison.java
@@ -0,0 +1,8 @@
+/*
+    *@desc : Line Comparison Computation Class
+ */
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line Comparison Computation");
+    }
+}
