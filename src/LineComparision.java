932c6c87fb03c0eb411892881e284d61af4cc6b8
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 19:58:43 +0530

    Add : Line Comparision Class Initialized

---
+++ src/LineComparision.java
@@ -0,0 +1,8 @@
+/*
+    *@desc : Line Comparision Computation Class
+ */
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line Comparision Computation");
+    }
+}
