06f11a43b6e57c311e0496c265608c83eff2e3ba
Sumit Jangid <sumitjangid1692153@gmail.com>
2023-11-27 20:19:16 +0530

    Add : UC 1 Line Class and Point Class

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -4,5 +4,7 @@
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to line Comparison Computation");
+        Line l = new Line(10 , 0 , 5, 0);
+        System.out.println(l.length());
     }
 }
